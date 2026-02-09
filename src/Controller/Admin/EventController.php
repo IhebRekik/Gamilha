509947d4f104250e542dfdafe5090df55efdfad7
Khawla Najai <khawla@example.com>
2026-02-09 19:48:03 +0100

    Modifications après l'ajout de recherche

--- src/Controller/Admin/EventController.php
+++ src/Controller/Admin/EventController.php
@@ -17,8 +17,19 @@ final class EventController extends AbstractController
     #[Route(name: 'admin_event_index', methods: ['GET'])]
     public function index(EventRepository $eventRepository): Response
     {
+        $games = $eventRepository->createQueryBuilder('e')
+            ->select('DISTINCT e.game')
+            ->orderBy('e.game', 'ASC')
+            ->getQuery()
+            ->getResult();
+        $games = array_column($games, 'game');
+        $events = $eventRepository->findAll();
+
+
+        // ou plus simple si tu as déjà tous les events :
         return $this->render('admin/event/index.html.twig', [
-            'events' => $eventRepository->findAll(),
+            'events' => $events,
+            'games' => $games
         ]);
     }
 
@@ -39,7 +50,7 @@ final class EventController extends AbstractController
         return $this->render('admin/event/new.html.twig', [
             'event' => $event,
             'form' => $form,
-            "edit"=>false
+            "edit" => false
         ]);
     }
 
@@ -66,14 +77,14 @@ final class EventController extends AbstractController
         return $this->render('admin/event/edit.html.twig', [
             'event' => $event,
             'form' => $form,
-            "edit"=>true
+            "edit" => true
         ]);
     }
 
     #[Route('/{id}', name: 'admin_event_delete', methods: ['POST'])]
     public function delete(Request $request, Event $event, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$event->getId(), $request->getPayload()->getString('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $event->getId(), $request->getPayload()->getString('_token'))) {
             $entityManager->remove($event);
             $entityManager->flush();
         }
---
+++ src/Form/ChatMessage1Type.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\ChatMessage;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ChatMessage1Type extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('content')
+            ->add('isRead')
+            ->add('sender', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+            ])
+            ->add('recipient', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => ChatMessage::class,
+        ]);
+    }
+}
